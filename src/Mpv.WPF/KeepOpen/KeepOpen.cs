b0b772c330669da98f4e38b2be9cb37f92c6b587
Aurel Hudec <hudec117@gmail.com>
2018-04-15 16:42:40 +0100

    Made KeepOpen default "No".

--- src/Mpv.WPF/KeepOpen/KeepOpen.cs
+++ src/Mpv.WPF/KeepOpen/KeepOpen.cs
@@ -3,14 +3,14 @@
 	public enum KeepOpen
 	{
 		/// <summary>
-		/// Do not unload media when it reaches the end and it is the last entry in the playlist.
-		/// </summary>
-		Yes,
-
-		/// <summary>
 		/// When the current media ends, play the next media or stop.
 		/// </summary>
 		No,
+
+		/// <summary>
+		/// Do not unload media when it reaches the end and it is the last entry in the playlist.
+		/// </summary>
+		Yes,
 
 		/// <summary>
 		/// Similar to "Yes" but it will not advance to the next entry in the playlist.
